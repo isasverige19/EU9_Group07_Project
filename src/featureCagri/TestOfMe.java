6440d4d79335ac2c97dd8a0d1a7e3225c88a31c6
CagriSh <104763269+CagriSh@users.noreply.github.com>
2022-07-31 21:27:42 +0200

    Created package featureCagri and TestOfMe class.

---
+++ src/featureCagri/TestOfMe.java
@@ -0,0 +1,9 @@
+package featureCagri;
+
+public class TestOfMe {
+
+    public static void main(String[] args) {
+        System.out.println("Hi guys");
+        System.out.println("I hope it is working");
+    }
+}
