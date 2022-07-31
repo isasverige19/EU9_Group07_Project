3f4225bac9c22b0bb01df40929e8b71c9e930a1a
isasverige19 <isaeraslan@gmail.com>
2022-07-31 21:17:17 +0200

    I adde some  new features

---
+++ src/featureIsaTwo/TestIsa.java
@@ -0,0 +1,11 @@
+package featureIsaTwo;
+
+public class TestIsa {
+    public static void main(String[] args) {
+
+
+        System.out.println("I have added some really good features :)");
+    }
+
+
+}
