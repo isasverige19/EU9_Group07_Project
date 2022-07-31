ab7445d919038a163d08a4c81740bb857ccc50a1
CagriSh <104763269+CagriSh@users.noreply.github.com>
2022-07-31 21:43:40 +0200

    Created package tryingToMakePullRequests and test01 class

---
+++ src/featureCagri/tryingToMakePullRequests/Test01.java
@@ -0,0 +1,11 @@
+package featureCagri.tryingToMakePullRequests;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+
+    }
+}
